5af0c7a513c0593241cebb2838d21ed55272189f
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-07-16 16:06:40 +0200

    Add pages

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,11 +1,38 @@
 <template>
     <div>
-        <p>product</p>
+        <h2>Product</h2>
+        <p>Nostrud occaecat fugiat excepteur eiusmod laboris incididunt non veniam. Proident Lorem excepteur cillum sunt
+            commodo esse. Lorem ad sunt non quis laboris sint excepteur ex ullamco. Ex pariatur ex aute sint Lorem magna
+            est do ad aliquip ullamco fugiat qui. Ut reprehenderit nisi amet veniam adipisicing reprehenderit excepteur
+            incididunt adipisicing id do ex veniam cillum. Culpa id velit esse amet nulla. Ullamco elit ullamco do ipsum
+            elit nisi ipsum id amet incididunt eu anim enim ut.
+        </p>
+        <p>Nostrud occaecat fugiat excepteur eiusmod laboris incididunt non veniam. Proident Lorem excepteur cillum sunt
+            commodo esse. Lorem ad sunt non quis laboris sint excepteur ex ullamco. Ex pariatur ex aute sint Lorem magna
+            est do ad aliquip ullamco fugiat qui. Ut reprehenderit nisi amet veniam adipisicing reprehenderit excepteur
+            incididunt adipisicing id do ex veniam cillum. Culpa id velit esse amet nulla. Ullamco elit ullamco do ipsum
+            elit nisi ipsum id amet incididunt eu anim enim ut.
+        </p>
     </div>
 </template>
 
 <script setup>
+definePageMeta({
+    layout: 'product'
+})
 
+await useFetch('https://fakestoreapi.com/products')
+
+window
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
