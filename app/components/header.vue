1e401fd23c7fde26086c78ecf89e45488d0d5308
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-07-16 22:32:26 +0200

    style: stylized button

--- app/components/header.vue
+++ app/components/header.vue
@@ -27,6 +27,20 @@
                             Blog</NuxtLink>
                     </li>
                 </ul>
+
+                <div class="lg:flex-1 flex items-center gap-1.5 justify-end">
+                    <NuxtLink
+                        class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-md text-sm gap-x-2 px-3 py-2 shadow-sm text-white dark:text-gray-900 bg-gray-900 hover:bg-gray-800 disabled:bg-gray-900 dark:bg-white dark:hover:bg-gray-100 dark:disabled:bg-white focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 items-center hidden lg:flex"
+                        to="/login">
+                        <span class="">Sign in</span>
+                    </NuxtLink>
+                    <NuxtLink
+                        class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-md text-sm gap-x-2 px-3 py-2 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700/50 dark:disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center"
+                        to="/login">
+                        <span class="">Get started</span>
+                        <icon name="i-heroicons-arrow-right-20-solid" class="flex-shrink-0 h-5 w-5" aria-hidden="true" />
+                    </NuxtLink>
+                </div>
             </nav>
         </header>
     </div>
@@ -36,6 +50,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
