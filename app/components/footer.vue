42e51419632ff61d37eba1fda850ccc1493e051d
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-07-17 14:56:30 +0200

    style: stylized components

---
+++ app/components/footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <footer class="bg-slate-800  shadow">
+            <div class="container mx-auto px-6 py-4">
+                <div class="flex items-center justify-between">
+                    <div class="text-white">© 2024 VotreSaaS. Tous droits réservés.</div>
+                    <div class="flex gap-6">
+                        <NuxtLink to="/"
+                            class="text-sm/6 font-semibold items-center gap-1 hover:text-[#00dc82] text-gray-900 dark:text-white transition duration-150">
+                            Politique de confidentialité</NuxtLink>
+                        <NuxtLink to="/"
+                            class="text-sm/6 font-semibold items-center gap-1 hover:text-[#00dc82] text-gray-900 dark:text-white transition duration-150">
+                            Conditions d'utilisation</NuxtLink>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
