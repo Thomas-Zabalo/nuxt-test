cdcfe62a77dec703f492a411a1de78c45554b8b9
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-07-17 16:01:04 +0200

    refactor: add dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  srcDir: 'app/',
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon"]
 })
